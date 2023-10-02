606d00d2eedd1157a1501daf41f1e3f3668baf6a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-02 19:50:56 -0300

    Added comments

--- Section 3/bigO.js
+++ Section 3/bigO.js
@@ -1,8 +1,10 @@
 const nemo = ["nemo"];
-
+//By using the nemo const an input the algorithm tends to be faster
 const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat",'nigel','squirt','darla','hank'];
+//By using the everyone const the algorithm tends to be a little bit slower than usual
 
 const large = new Array(100000).fill('nemo');
+//By using the large const it becomes very slower
 
 function findNemo(array) {
   let t0 = performance.now();
