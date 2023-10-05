ea44e70d355a99b91c5afa38c1a168a6c99ada24
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-04 22:51:00 -0300

    Corrected Big O notation in exercise 2

--- Section 3/exercises/BigOExercise2.js
+++ Section 3/exercises/BigOExercise2.js
@@ -3,14 +3,14 @@ function anotherFunChallenge(input) {
     let a = 5; // O(1)
     let b = 10; // O(1)
     let c = 50; // O (1)
-    for (let i = 0; i < input; i++)  // O(n)
+    for (let i = 0; i < input; i++)  
     {
       let x = i + 1; // O (n)
       let y = i + 2; // O (n)
       let z = i + 3; // O (n)
   
     }
-    for (let j = 0; j < input; j++) // O(n)
+    for (let j = 0; j < input; j++) 
     {
       let p = j * 2; // O(n)
       let q = j * 2; // O(n)
@@ -19,5 +19,5 @@ function anotherFunChallenge(input) {
     // 4 0(1) + 7 O(n)
     // 4 + n + n + n + n + n + n + n
     // 4 + 7n
-    // Big O notation is O(4 + 7n) or simplifying O(n)
+    // Big O notation is O(4 + 5n) or simplifying O(n)
   }
