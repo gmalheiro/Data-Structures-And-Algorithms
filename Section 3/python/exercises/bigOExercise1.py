31a7c9876d3dc728bf7e641b58d9b45baf2cb367
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-05 15:10:14 -0300

    bigOExercise1 done

---
+++ Section 3/python/exercises/bigOExercise1.py
@@ -0,0 +1,15 @@
+def funChallenge(input):
+    a = 10 #O(1)
+    a = 50 + 3 #O(1)
+
+    input_length = len(input)  # O(1) Assuming 'input' is your input array
+
+    for i in range(input_length): # O(n)
+        another_function() #O(n)
+        stranger = True #O(n)
+        a += 1 #O(n)
+
+    return a
+
+def another_function():
+    print()
