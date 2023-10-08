217653c999c958d0ad8eb89da9cf002d3034305b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-08 17:14:18 -0300

    Big O third rule lecture done

---
+++ Section 3/js/bigOThirdRule.js
@@ -0,0 +1,38 @@
+function aCompressBoxesTwice (boxes){
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+
+
+    boxes.forEach(function(boxes) {
+        console.log(boxes);
+    });
+}
+
+function bCompressBoxesTwice (boxes, boxes2){
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+
+
+    boxes2.forEach(function(boxes) {
+        console.log(boxes);
+    });
+}
+
+/* 
+    The big o of example a is:
+    O(2n)
+    But as we drop the constants it becomes:
+    O(n)
+
+
+    But for example b it's not, because the third rule states that 
+    different terms for inputs and 
+
+    the first loop depends on how big the first input is 
+    and the second loop depends on how big the second input is 
+
+    So the big O notation of the example b would be:
+    O(a + b)
+*/
