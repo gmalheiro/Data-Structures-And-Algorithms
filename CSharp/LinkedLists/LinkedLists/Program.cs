0e938307c62f1349bc6201eca5d58354a7be00f9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-11-25 23:02:53 -0300

    Finished implementing linked list

--- CSharp/LinkedLists/LinkedLists/Program.cs
+++ CSharp/LinkedLists/LinkedLists/Program.cs
@@ -1,10 +1,63 @@
 ﻿namespace LinkedLists
 {
+
+    class LinkedListNode
+    {
+        public int Data { get; set; }
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        int count;
+        public LinkedListNode Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+            count = 0;
+        }
+
+        public void AddNode(int data)
+        {
+            LinkedListNode node = new LinkedListNode(data);
+            node.Next = Head;
+            Head = node;
+            count++;
+        }
+
+        public void PrintList()
+        {
+            var runner = Head;
+            while (runner != null)
+            {
+                Console.WriteLine(runner.Data);
+                runner = runner.Next;
+            }
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var linkedList = new LinkedList();
+            linkedList.AddNode(1);
+            linkedList.AddNode(2);
+            linkedList.AddNode(3);
+            linkedList.AddNode(4);
+            linkedList.AddNode(5);
+            linkedList.AddNode(6);
+            linkedList.AddNode(7);
+            linkedList.AddNode(8);
+
+            linkedList.PrintList();
         }
     }
 }
